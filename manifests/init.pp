851f2559c6918964ae1b9f1e03bc767fd3e94905
Jared Nelson <jared@ajpadilla.com>
2013-10-14 19:41:17 -0500

    don't use config_file_init under OpenBSD

--- manifests/init.pp
+++ manifests/init.pp
@@ -457,17 +457,19 @@ class nrpe (
     audit   => $nrpe::manage_audit,
   }
 
-  file { 'nrpe.init':
-    ensure  => $nrpe::manage_file,
-    path    => $nrpe::config_file_init,
-    mode    => $nrpe::config_file_mode,
-    owner   => $nrpe::config_file_owner,
-    group   => $nrpe::config_file_group,
-    require => Package['nrpe'],
-    notify  => $nrpe::manage_service_autorestart,
-    content => $nrpe::manage_file_init_content,
-    replace => $nrpe::manage_file_replace,
-    audit   => $nrpe::manage_audit,
+  if $nrpe::config_file_init != '' {
+    file { 'nrpe.init':
+      ensure  => $nrpe::manage_file,
+      path    => $nrpe::config_file_init,
+      mode    => $nrpe::config_file_mode,
+      owner   => $nrpe::config_file_owner,
+      group   => $nrpe::config_file_group,
+      require => Package['nrpe'],
+      notify  => $nrpe::manage_service_autorestart,
+      content => $nrpe::manage_file_init_content,
+      replace => $nrpe::manage_file_replace,
+      audit   => $nrpe::manage_audit,
+    }
   }
 
   file { 'nrpe.dir':
